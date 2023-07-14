9ca42496536df156ff758a7de01e6a3bcb64d692
sisithajayawardhane <sisithajayawardhane@gmail.com>
2023-07-14 23:09:32 +0530

    login component is created

--- apps/dilagro/src/app/app.component.ts
+++ apps/dilagro/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule,LoginComponent, CommonModule],
   selector: 'org-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
