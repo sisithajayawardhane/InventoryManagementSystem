a1927ee0d4dcb98c35ab47eb348ef3d2281134c3
sisithajayawardhane <sisithajayawardhane@gmail.com>
2023-07-14 22:49:44 +0530

    project structure created

--- apps/dilagro/src/app/app.component.ts
+++ apps/dilagro/src/app/app.component.ts
@@ -1,14 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule, CommonModule],
   selector: 'org-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'dilagro';
+  name = 'sisitha';
+  frameworks = ['Angular', 'React', 'Vue'];
+
+  onClick() {
+    this.name = "Dhananjaya";
+  }
 }
